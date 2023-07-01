27f64c3b6d212c7d27d10cee9f0d9cade1d7dc21
Matheus <matheus.a.v.b@gmail.com>
2023-06-30 21:27:34 -0300

    feat: swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Valorant API')
+    .setDescription('REST API developed with Nest.js, Prisma and PostgreSQL.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
 
   app.enableShutdownHooks();
 
